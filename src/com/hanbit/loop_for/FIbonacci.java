adc41a7a6d69891bfd3b8e3a7dda6a8af712b000
Songsanghun <loveiamsong@gmail.com>
2016-12-26 18:38:24 +0900

    Add Comment

--- src/com/hanbit/loop_for/FIbonacci.java
+++ src/com/hanbit/loop_for/FIbonacci.java
@@ -1,8 +1,11 @@
 package com.hanbit.loop_for;
-
+import java.util.Scanner;
 public class FIbonacci {
 	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
 		//1, 1, 2, 3, 5, 8, 13, 21, 34, 55 의 합
+        System.out.println("어느항까지의 합을 원하는지 항을 입력해주세요.");
+        int i = s.nextInt();
 		int a=1, b=1, c=2, sum=2;
 		for(int i=3;i<6;i++){
 			c=a+b;
@@ -10,6 +13,10 @@ public class FIbonacci {
 			b=c+a;
 			sum+=a+b+c;
 		}
-		System.out.println("1, 1, 2, 3, 5, 8, 13, 21, 34, 55 의 합은 : "+(sum-b));
+		if(i%3==0){
+			System.out.println(i+"항까지의 합은"+sum);
+		}else if(i%3==1){
+			System.out.println(i+"항까지의 합은"+(sum-b));
+		}else{System.out.println(i+"항까지의 합은"+(sum-b-c));}
 		}
 	}
